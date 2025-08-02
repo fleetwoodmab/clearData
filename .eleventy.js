421609165f4d0eb7f18673510901317a30f7d16f
Marb <julyhope@proton.me>
2025-08-02 19:58:07 +0200

    redirects

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
 
 
   return {
