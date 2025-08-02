788069a5028b99a89059b96eaa43ac0159ec3b88
Marb <julyhope@proton.me>
2025-08-02 17:33:36 +0200

    brandnew

---
+++ src/_data/translations.js
@@ -0,0 +1,19 @@
+module.exports = {
+  en: {
+    about: "About",
+    ourMission: "Our Mission",
+    location: "Where to Find Us",
+    contact: "Contact",
+    careers: "Careers"
+  },
+  fr: {
+    about: "A propos",
+    ourMission: "Notre Mission",
+    location: "Ou nous trouver",
+    contact: "Contact",
+    careers: "Carrieres"
+  }
+};
+
+
+
