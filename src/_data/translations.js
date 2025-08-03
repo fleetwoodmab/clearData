d78ec1bffab4c2a6c91da913c4f374eccfe552d6
Marb <julyhope@proton.me>
2025-08-03 14:51:04 +0200

    ninetyfive

--- src/_data/translations.js
+++ src/_data/translations.js
@@ -2,16 +2,22 @@ module.exports = {
   en: {
     about: "About",
     ourMission: "Our Mission",
-    location: "Where to Find Us",
+    location: "Network",
     contact: "Contact",
-    careers: "Careers"
+    careers: "Careers",
+
+    lingo: "Languages",
+    aboutFooter: "Bilingual data consulting agency providing accurate, structured, and scalable data services for machine learning, research, and enterprise analytics through certified experts.",
   },
   fr: {
-    about: "A propos",
+    about: "À propos",
     ourMission: "Notre Mission",
-    location: "Ou nous trouver",
+    location: "Réseau",
     contact: "Contact",
-    careers: "Carrieres"
+    careers: "Carrières",
+
+    lingo: "Langues",
+    aboutFooter: "Agence de conseil en données bilingue offrant des services précis, structurés et évolutifs pour l’apprentissage automatique, la recherche et l’analytique d’entreprise via des experts certifiés.",
   }
 };
 
