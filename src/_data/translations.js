4b896db4595e89bd87199f304cf1eeec1ad86057
Marb <julyhope@proton.me>
2025-08-03 16:35:19 +0200

    test99

--- src/_data/translations.js
+++ src/_data/translations.js
@@ -1,20 +1,30 @@
 module.exports = {
   en: {
     about: "About",
-    ourMission: "Our Mission",
+    ourMission: "Services",
     location: "Network",
     contact: "Contact",
     careers: "Careers",
+
+    aboutNav: "about",
+    servicesNav: "services",
+    networkNav: "network",
+    careersNav: "careers",
 
     lingo: "Languages",
     aboutFooter: "Bilingual data consulting agency providing accurate, structured, and scalable data services for machine learning, research, and enterprise analytics through certified experts.",
   },
   fr: {
     about: "À propos",
-    ourMission: "Notre Mission",
+    ourMission: "Services",
     location: "Réseau",
     contact: "Contact",
     careers: "Carrières",
+
+    aboutNav: "a-propos",
+    servicesNav: "nos-services",
+    networkNav: "reseau",
+    careersNav: "nous-rejoindre",
 
     lingo: "Langues",
     aboutFooter: "Agence de conseil en données bilingue offrant des services précis, structurés et évolutifs pour l’apprentissage automatique, la recherche et l’analytique d’entreprise via des experts certifiés.",
